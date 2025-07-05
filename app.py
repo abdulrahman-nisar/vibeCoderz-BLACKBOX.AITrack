67bfa2c82820fbecc58223f13d290c6acd064399
abdulrahman-nisar <abdulrahmannisar6@gmail.com>
2025-07-05 16:07:56 +0500

    new changes

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from api import ollama_ai,groq_ai
 
-completion = groq_ai("What is the capital of France?")
-for chunk in completion:
-    print(chunk.choices[0].delta.content or "", end="")
 
-var = ollama_ai("What is the capital of France?")
-print(var)  
+
+
+# completion = groq_ai("What is the capital of France?")
+# for chunk in completion:
+#     print(chunk.choices[0].delta.content or "", end="")
+
+# var = ollama_ai("What is the capital of France?")
+# print(var)  
 
