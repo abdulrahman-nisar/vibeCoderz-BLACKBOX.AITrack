9decec3e71518b0b3eeffb6bbe1dcd0558b5aeec
MRehanArshad <f233098@cfd.nu.edu.pk>
2025-07-05 16:38:04 +0500

    Adding context to the API

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from api import ollama_ai,groq_ai
 
-completion = groq_ai("What is the capital of France?")
+completion = groq_ai("We want to make photo recognition app?")
 for chunk in completion:
     print(chunk.choices[0].delta.content or "", end="")
 
-var = ollama_ai("What is the capital of France?")
-print(var)  
-
+# var = ollama_ai("What is the capital of France?")
+# print(var)
